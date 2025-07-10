376f97082e2c394fa0ca900531a7005819a4f2f8
pyGuy152 <sarveshsabe@gmail.com>
2025-07-10 18:59:22 -0500

    Update .gitignore, add psycopg2-binary to requirements, and implement database utility functions

---
+++ database/utils.py
@@ -0,0 +1,36 @@
+import psycopg2, os, time
+from psycopg2 import Error
+from psycopg2.extras import RealDictCursor 
+from dotenv import load_dotenv
+
+load_dotenv()
+db_user = os.getenv("DB_User")
+db_pass = os.getenv("DB_User_PASS")
+
+def sqlQuery(sql: str, params: tuple, fetch: int = 0):
+    while True:
+        try:
+            conn = psycopg2.connect(database='echoverse',user=db_user,password=db_pass,host='localhost',port='5432',cursor_factory=RealDictCursor)
+            cur = conn.cursor()
+            break
+        except Error as e:
+            if conn:
+                conn.close()
+            print(f"Error connecting to DB: {e}")
+            time.sleep(5)
+    try:
+        if fetch > 1:
+            cur.execute(f"{sql} LIMIT {fetch};",params)
+            out = cur.fetchall()
+        elif fetch == 1:
+            cur.execute(f"{sql}",params)
+            out = cur.fetchone()
+        else:
+            cur.execute(f"{sql}",params)
+            out = None
+    except Exception as e:
+        print(e)
+        raise 'sql error'
+    conn.commit()
+    conn.close()
+    return out
